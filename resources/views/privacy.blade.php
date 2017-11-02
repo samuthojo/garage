b3a821b9b728e6b55397aec5aba1391b6e1ed357
samuthojo <samuthojo@gmail.com>
2017-11-02 20:41:21 +0300

    modified:   resources/views/privacy.blade.php

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,1 +1,10 @@
-<h4>Privacy Policy Goes Here</h4>
+<!doctype html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Privacy</title>
+</head>
+<body style="align-content: center;">
+  <h4 style="color: #31708f">Privacy Policy Goes Here</h4>
+</body>
+</html>
