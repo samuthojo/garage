60b0e7ba35145765e4f3a1ed197e2428ea97724f
samuthojo <samuthojo@gmail.com>
2017-11-02 12:32:09 +0300

    modified:   resources/views/modals/edit_service.blade.php

--- resources/views/modals/edit_service.blade.php
+++ resources/views/modals/edit_service.blade.php
@@ -58,9 +58,7 @@
                  @endforeach
                @endif
              </select>
-             <span class="select_loader" style="display: none;">
-               <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
-             </span>
+             <i class="fa fa-spinner fa-spin fa-3x fa-fw select_loader" style="display: none; float: left"></i>
            </div>
            <span  class="text-danger col-sm-offset-2" id="model_error3"></span>
          </div>
