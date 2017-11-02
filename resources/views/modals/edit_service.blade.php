43b707eb177517741ea04a5944acb56a172a6842
samuthojo <samuthojo@gmail.com>
2017-11-02 11:55:28 +0300

    modified:   resources/views/modals/edit_service.blade.php

--- resources/views/modals/edit_service.blade.php
+++ resources/views/modals/edit_service.blade.php
@@ -51,7 +51,7 @@
                  @endforeach
                @endif
              </select>
-             <i class="fa fa-spinner fa-spin fa-3x fa-fw select_loader"
+             <i class="fa fa-spinner fa-spin fa-2x fa-fw select_loader"
               style="display: none;"></i>
              <span  class="text-danger col-sm-offset-2" id="model_error3"></span>
          </div>
