995781a54951ac9e5a36d24b27b777f548b8ca02
samuthojo <samuthojo@gmail.com>
2018-01-29 13:55:59 +0300

    modified:   resources/views/tab_loader.blade.php

--- resources/views/tab_loader.blade.php
+++ resources/views/tab_loader.blade.php
@@ -1,4 +1,4 @@
-<div class="tab_loader">
+<div class="tab_loader" style="margin-top: 15px;">
   <span>
     <i class="fa fa-spinner fa-spin fa-3x fa-fw"
       style="color: #ff9720 !important;"></i>
